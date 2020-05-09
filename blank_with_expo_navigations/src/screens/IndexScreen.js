f4c8955b42a95f42803050570988e8f5e65e3b9a
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-09 12:05:58 +0300

    eka commit

---
+++ blank_with_expo_navigations/src/screens/IndexScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const IndexScreen = ()=>{
+
+
+ return (
+     <Text>Index sivu</Text>
+ ) 
+}
+
+
+const styles= StyleSheet.create({});
+
+export default IndexScreen;
