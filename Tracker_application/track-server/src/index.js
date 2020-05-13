0bfae064ad691a3c2c198e03cd34651a5f5c241c
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-13 14:13:41 +0300

    installointia ja setuppia.

--- Tracker_application/track-server/src/index.js
+++ Tracker_application/track-server/src/index.js
@@ -1,12 +1,34 @@
-const express =  require ('express');
-
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.get('/', (req, res) => {
+
+const mongoUri = 'mongodb+srv://admin:Vitunsalasana666@cluster0-nlbxp.mongodb.net/test?retryWrites=true&w=majority'
+
+//otetaan mongoose käyttöön
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+
+//testataan et mongoose on yhdistetty
+mongoose.connection.on('connected', () =>{
+    console.log('Connected to Mongo instance');
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error('mongoose error');
+});
+
+
+
+app.get('/', (req, res)=>{
     res.send('hi there');
 });
 
 
-app.listen = (3001, () => {
-    console.log('listening on port 3001');
+app.listen(3000,()=>{
+    console.log('listening port 3000')
 });
