67b069b3e64aa5e8b293921a8824f7407ca82ae3
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-14 10:09:14 +0300

    Lisätty reitin luonti.

--- Tracker_application/track-server/routes/trackRoutes.js
+++ Tracker_application/track-server/routes/trackRoutes.js
@@ -21,5 +21,21 @@ const router =express.Router();
     res.send(tracks);
 });
 
+//oletetaan et post req lähettää nimen ja saman rakenteen kun mitä tietokanta mallissa on.(rakennetaan se niin)
+router.post('/tracks', async(req, res) =>{
+    const {name, locations} = req.body;
+    if (!name || !locations){
+        return ({ error: 'You must provide a name and locations'});
+    }
+    try{
+        const track = new Track({name, locations, userId: req.user._id});
+        await track.save(); //this could fail if user input is garbage
+        res.send(track);
+    } catch(err){
+        res.status(422).send({ error: err.message});
+    }
+
+});
+
 
 module.exports = router;
